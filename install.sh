ba24d8ef4ed9eb68dd72791d00fec6d1ae73a3b5
Eric leo william Schneller <eschnell@z1r5p2.le-101.fr>
2018-07-23 13:29:20 +0200

    Edition finale

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
- #!/bin/sh
+#!/bin/sh
 
 set -e
-git clone https://github.com/eric123911/segfault.git ~/.segfault
+env git clone https://github.com/eric123911/segfault.git ~/.segfault
 echo "## fonction pour annoncer le saigue-faulte
 source ~/.segfault/resrc/catcher.zsh" >> ~/.zshrc
 sleep 1
-echo
+echo " "
 echo "IMPORTATION TERMINÉE.."
 echo "Veuillez maintenant redémarrer votre Terminal pour que celui-ci détecte vos segfaults :)"
